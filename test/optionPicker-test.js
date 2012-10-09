443c6b3fcbd63ba7d5617b0e4c6aec3fb5dbcbd2
Ian Hill <ianhill@buymeasoda.com>
2012-10-09 22:32:38 +1000

    Add test setup function to create an optionPicker instance

--- test/optionPicker-test.js
+++ test/optionPicker-test.js
@@ -2,6 +2,11 @@ var assert = buster.assert;
 
 buster.testCase('jQuery optionPicker', {
     
+    setUp: function () {
+        this.select = $('select');
+        this.optionPicker = this.select.optionPicker().data('sodaOptionPicker');
+    },
+
     'exists on the jQuery prototype': function () {
         assert.isFunction($.fn.optionPicker);
     },
