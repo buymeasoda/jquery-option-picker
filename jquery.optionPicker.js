c679889098f6c485963fd09a01cae52cbed3374c
Ian Hill <ianhill@buymeasoda.com>
2013-05-27 15:54:56 +1000

    Separate view update from set function

--- jquery.optionPicker.js
+++ jquery.optionPicker.js
@@ -25,15 +25,18 @@
             this.widget = this.el.container;
             this.el.increment.on(eventType, $.proxy(this.increment, this));
             this.el.decrement.on(eventType, $.proxy(this.decrement, this));
-            this.set(this.el.select.prop('selectedIndex'));
+            this.update(this.el.select.prop('selectedIndex'));
             this.el.select.hide().after(this.el.container);
+        },
+        update: function (index) {
+            this.el.decrement.toggleClass(this.settings.cssClass + '-disabled', index === 0);
+            this.el.increment.toggleClass(this.settings.cssClass + '-disabled', index === this.size - 1);
+            this.el.value.text(this.el.options.eq(index).text());
         },
         set: function (index) {
             if (index >= 0 && index < this.size) {
                 this.el.select.prop('selectedIndex', index);
-                this.el.decrement.toggleClass(this.settings.cssClass + '-disabled', index === 0);
-                this.el.increment.toggleClass(this.settings.cssClass + '-disabled', index === this.size - 1);
-                this.el.value.text(this.el.options.eq(index).text());
+                this.update(index);
             }
         },
         increment: function () {
