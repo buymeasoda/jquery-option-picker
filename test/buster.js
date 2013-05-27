28902f925c0a229b7c7d734f4312105798c83512
Ian Hill <ianhill@buymeasoda.com>
2013-05-27 15:38:00 +1000

    Update jQuery to current version

--- test/buster.js
+++ test/buster.js
@@ -3,7 +3,7 @@ var config = module.exports;
 config['optionPicker Test'] = {
     environment: 'browser',
     rootPath: '../',
-    libs: ['lib/jquery-1.8.2.min.js'],
+    libs: ['lib/jquery-1.10.0.min.js'],
     sources: ['jquery.optionPicker.js'],
     tests: ['test/*-test.js'],
     resources: [
